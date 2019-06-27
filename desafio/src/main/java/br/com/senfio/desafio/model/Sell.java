701c8b6443c52b732637f64ce3a98f2ab706caad
Allyson Manoel <amnv@cin.ufpe.br>
2019-06-26 23:07:22 -0300

    Adicionando classes básicas

--- desafio/src/main/java/br/com/senfio/desafio/model/Sell.java
+++ desafio/src/main/java/br/com/senfio/desafio/model/Sell.java
@@ -1,5 +1,54 @@
 package br.com.senfio.desafio.model;
 
-public class Vendas {
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Sell {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private Date date;
+	
+	@OneToMany(targetEntity=Product.class, fetch=FetchType.EAGER)
+    @JoinColumn(name="cliente_id")
+	private List<Product> produtos;
+
+	public Sell() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public List<Product> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Product> produtos) {
+		this.produtos = produtos;
+	}
 
 }
