b54d2bd63a71ff7d799c277db396bdd33e0d4fd3
Allyson Manoel <amnv@cin.ufpe.br>
2019-06-26 23:12:17 -0300

    Adicionando configurações

--- desafio/src/main/java/br/com/senfio/desafio/App.java
+++ desafio/src/main/java/br/com/senfio/desafio/App.java
@@ -1,13 +1,28 @@
 package br.com.senfio.desafio;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootApplication
+@EntityScan(basePackages = {
+            "br.com.senfio.model"
+            })
+@EnableJpaRepositories(basePackages = {
+            "br.com.senfio.repository"
+            })
+@ComponentScan(basePackages = {"br.com.senfio"})
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@ContextConfiguration({"classpath*:spring/applicationContext.xml"})
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	SpringApplication.run(App.class, args);
     }
 }
